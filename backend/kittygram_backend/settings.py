412d765e5dc45a616c201039b592c34cb15bf47b
Роман Морозевич <romamorozevich@yandex.ru>
2025-12-07 12:58:56 +0300

    import sort1

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,9 +5,9 @@ from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
+SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
