abf7fd6e0bb9d5d803c27525aed37507386ee759
Роман Морозевич <romamorozevich@yandex.ru>
2025-12-07 13:31:36 +0300

    dsaф

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
 from rest_framework import routers
 
 from cats.views import AchievementViewSet, CatViewSet
