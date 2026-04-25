6d83cfeb88d0ffdf8020bc704bf6594f91f864a5
Roman Morozevich <romamorozevich@yandex.ru>
2026-04-26 00:55:27 +0300

    terraform added v2

--- infra/network.tf
+++ infra/network.tf
@@ -4,7 +4,7 @@ resource "yandex_vpc_network" "kittygram" {
 
 resource "yandex_vpc_subnet" "kittygram" {
   name           = "kittygram-subnet"
-  zone           = var.yc_zone
+  zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.kittygram.id
   v4_cidr_blocks = ["10.10.0.0/24"]
 }
